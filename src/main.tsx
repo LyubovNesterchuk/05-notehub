beedb53fb7c8b9faec6a012e31ebbe3438aaaa92
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-08-11 18:39:28 +0300

    components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import "modern-normalize";
+
+import './index.css'
+import App from './components/App/App.tsx'
+
+const queryClient=new QueryClient();
+createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}> 
+       <App />
+       <ReactQueryDevtools/>
+    </QueryClientProvider>
   </StrictMode>,
 )
